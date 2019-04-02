f40350ded99a9a73fc07369cdcbf478301e20640
Дмитрий Багаев <sequensor@mail.ru>
2019-04-02 18:18:27 +0600

    add authorization check

--- taskmanager/src/main/java/it/sevenbits/taskmanager/web/controllers/TasksController.java
+++ taskmanager/src/main/java/it/sevenbits/taskmanager/web/controllers/TasksController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.net.URI;
 import java.util.Collection;
 
@@ -37,12 +39,16 @@ public class TasksController {
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<Task> createTask(final @RequestParam("text") String text) {
+    public ResponseEntity<Task> createTask(final @RequestParam("text") String text,
+                                           final HttpServletResponse response) {
         Task createdTask = taskRepository.createTask(text);
         if (createdTask == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).
                     contentType(MediaType.APPLICATION_JSON).build();
         }
+        Cookie cookie = new Cookie("sessionId", createdTask.getId());
+        cookie.setMaxAge(3600);
+        response.addCookie(cookie);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .location(URI.create(String.format("/tasks/%s", createdTask.getId())))
